503151a6caa5eafe677dfb894df3e86484a5a3e9
K-sovereign <terrell.durant@outlook.com>
2025-08-23 13:53:48 -0800

    add inital script

---
+++ scripts/docker-portainer-init.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+apt update && upgrade -y
+
+apt install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/$(. /etc/os-release && echo "$ID")/gpg | \
+    gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/$(. /etc/os-release && echo "$ID") \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt update
+
+apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin bpytop
+
+systemctl enable docker
+systemctl start docker
+
+mkdir -p /opt/portainer
+cd /opt/portainer
+
+cat <<EOF > docker-compose.yml
+version: '3.8'
+
+services:
+  portainer:
+    image: portainer/portainer-ce:latest
+    container_name: portainer
+    restart: always
+    ports:
+      - "8000:8000"
+      - "9443:9443"
+    volumes:
+      - /var/run/docker.sock:/var/run/docker.sock
+      - portainer_data:/data
+
+volumes:
+  portainer_data:
+EOF
+
+docker compose up -d
