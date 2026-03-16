3db99de0c4b361c34a06f07cff0547015a3cbe19
King Sovereign <terrell.durant@outlook.com>
2026-03-15 19:46:24 -0800

    Add script to install Terraform on Ubuntu
    Signed-off-by: King Sovereign <terrell.durant@outlook.com>

---
+++ scripts/terra-init.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# 1. Update package information and install utility dependencies
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
+
+# 2. Download and install the official HashiCorp GPG key
+curl -fsSL https://apt.releases.hashicorp.com/gpg | \
+sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+# 3. Verify the key's fingerprint
+gpg --no-default-keyring \
+--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--fingerprint
+
+# 4. Add the official HashiCorp repository to the system
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+# 5. Update package information again and install Terraform
+sudo apt-get update && sudo apt-get install terraform -y
+
+# 6. Verify that the installation was successful
+terraform -help
